53d2a2900fc63fe581da8f6c7a9110a73a1eb5d8
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-18 18:14:09 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 {
     internal class Program
     {
-      
+
+        enum WeekDays
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+
+        }
         static void Main(string[] args)
         {
+            #region Q1
 
-            Console.WriteLine("Hello World");
+            for (int i = 0; i < 7; i++)
+            {
+                WeekDays wd = (WeekDays)i;
+                Console.WriteLine(wd);
+            }
+            #endregion
         }
     }
 }
