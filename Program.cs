a652319ea67d8f991a5ed8a1200e8e59ee9ef18b
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-18 18:42:13 +0300

    Question 4

--- Program.cs
+++ Program.cs
@@ -12,6 +12,12 @@
         {
           None,  Spring,Summer, Autumn, Winter
         }
+
+        [Flags]
+        enum permission
+        {
+            Read=1, write=2, Delete=4, Execute=8
+        }
         static void Main(string[] args)
         {
             #region Q1
@@ -24,28 +30,33 @@
             #endregion
 
             #region Q2
-            Console.WriteLine("Enter Season Name : ");
-            try
-            {
-                Season season = (Season)Enum.Parse(typeof(Season), Console.ReadLine(), true);
-                switch (season)
-                {
-                    case Season.Spring:
-                        Console.WriteLine("march to may");
-                        break;
-                    case Season.Summer:
-                        Console.WriteLine("june to august");
-                        break;
-                    case Season.Autumn:
-                        Console.WriteLine("September to November");
-                        break;
-                    case Season.Winter:
-                        Console.WriteLine("December to February)");
-                        break;
-                }
-            }
-            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
-           
+            //Console.WriteLine("Enter Season Name : ");
+            //try
+            //{
+            //    Season season = (Season)Enum.Parse(typeof(Season), Console.ReadLine(), true);
+            //    switch (season)
+            //    {
+            //        case Season.Spring:
+            //            Console.WriteLine("march to may");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("june to august");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("September to November");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("December to February)");
+            //            break;
+            //    }
+            //}
+            //catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+            #endregion
+
+            #region Q4
+            permission p = (permission)13;
+            Console.WriteLine(p);
             #endregion
         }
     }
