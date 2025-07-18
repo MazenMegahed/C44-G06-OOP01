c48bcecb8ca0046013e73a44a9617840ea678859
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-18 18:39:04 +0300

    Question 2

--- Program.cs
+++ Program.cs
@@ -8,15 +8,44 @@
             Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
 
         }
+        enum Season
+        {
+          None,  Spring,Summer, Autumn, Winter
+        }
         static void Main(string[] args)
         {
             #region Q1
 
-            for (int i = 0; i < 7; i++)
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    WeekDays wd = (WeekDays)i;
+            //    Console.WriteLine(wd);
+            //}
+            #endregion
+
+            #region Q2
+            Console.WriteLine("Enter Season Name : ");
+            try
             {
-                WeekDays wd = (WeekDays)i;
-                Console.WriteLine(wd);
+                Season season = (Season)Enum.Parse(typeof(Season), Console.ReadLine(), true);
+                switch (season)
+                {
+                    case Season.Spring:
+                        Console.WriteLine("march to may");
+                        break;
+                    case Season.Summer:
+                        Console.WriteLine("june to august");
+                        break;
+                    case Season.Autumn:
+                        Console.WriteLine("September to November");
+                        break;
+                    case Season.Winter:
+                        Console.WriteLine("December to February)");
+                        break;
+                }
             }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+           
             #endregion
         }
     }
